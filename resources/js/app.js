34893b2e9f9c99d11ab05ca182486823ff6cadff
Evgeny Kuzmin <skchel174@gmail.com>
2023-11-27 22:54:31 +0500

    Refactored app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,39 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import {createSSRApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title(title) {
+    return title;
+  },
+
+  resolve(name) {
+    const path = `./Pages/${name}.vue`;
+    const pages = import.meta.glob('./Pages/**/*.vue');
+
+    return resolvePageComponent(path, pages)
+  },
+
+  setup({el, App, props, plugin}) {
+    const rootComponent = {
+      render: () => h(App, props)
+    };
+
+    return createSSRApp(rootComponent)
+      .use(plugin)
+      .use(ZiggyVue, Ziggy)
+      .mount(el);
+  },
+
+  progress: {
+    // The delay after which the progress bar will appear, in milliseconds...
+    delay: 0,
+
+    // The color of the progress bar...
+    color: '#29d',
+  },
 });
