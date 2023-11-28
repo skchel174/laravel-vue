98946651ac4b4a89edf41bfb926cf0cf5a42d509
Evgeny Kuzmin <skchel174@gmail.com>
2023-11-28 14:38:00 +0500

    Added faker property to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
+use Faker\Generator;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected Generator $faker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = resolve(Generator::class);
+    }
 }
