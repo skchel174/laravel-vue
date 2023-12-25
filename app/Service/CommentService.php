b3c537e60c88219135058098a3a87d5a552aceef
Evgeny Kuzmin <skchel174@gmail.com>
2023-12-25 16:59:12 +0500

    Created CommentService

---
+++ app/Service/CommentService.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Events\Article\CreateComment;
+use App\Models\Article\Article;
+use App\Models\Comment\Comment;
+use App\Models\User\User;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class CommentService
+{
+    public function __construct(
+        private readonly StatefulGuard $authService,
+        private readonly Dispatcher $eventDispatcher,
+    ) {
+    }
+
+    public function createForArticle(Article $article, string $text): Comment
+    {
+        /** @var User $user */
+        $user = $this->authService->user();
+
+        $comment = Comment::createForArticle($article, $user, $text);
+
+        $this->eventDispatcher->dispatch(new CreateComment($comment));
+
+        return $comment;
+    }
+}
