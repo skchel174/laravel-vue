ae9c28b5ba8979b8192ba2d925a219f57f89f2a3
Evgeny Kuzmin <skchel174@gmail.com>
2024-01-22 11:55:43 +0500

    Created article media table migration

---
+++ database/migrations/2024_01_17_121908_create_article_media_table.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('article_media', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id');
+            $table->timestamps();
+
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('CASCADE');
+        });
+
+        Schema::table('articles', function (Blueprint $table) {
+            $table->foreignId('article_media_id')
+                ->nullable()
+                ->after('feed_image');
+
+            $table->foreign('article_media_id')
+                ->references('id')
+                ->on('article_media')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropColumns('articles', 'article_media_id');
+
+        Schema::dropIfExists('article_media');
+    }
+};
