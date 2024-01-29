38837ca9ea939602f52b6fd84d3906506522f391
Evgeny Kuzmin <skchel174@gmail.com>
2024-01-29 13:03:26 +0500

    Added russian localization for categories and topics, created russion lang files

--- database/seeders/TopicsSeeder.php
+++ database/seeders/TopicsSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\Category\Category;
+use App\Models\Localization\Locale;
 use App\Models\Topic\Topic;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -13,19 +14,23 @@ class TopicsSeeder extends Seeder
 {
     public function run(): void
     {
-        foreach ($this->getCategories() as $category => $topics) {
-            $category = Category::factory()->create([
-                'name' => $category,
-                'slug' => Str::slug($category),
-            ]);
-
-            foreach ($topics as $topic => $icon) {
-                Topic::factory()->create([
-                    'name' => $topic,
-                    'slug' => Str::slug($topic),
-                    'icon' => 'topic/' . $icon,
-                    'category_id' => $category,
+        foreach ($this->getCategories() as $category => $categoryProps) {
+            $category = Category::factory()
+                ->withLocalization(Locale::Ru, $categoryProps['localization'])
+                ->create([
+                    'name' => $category,
+                    'slug' => Str::slug($category),
                 ]);
+
+            foreach ($categoryProps['topics'] as $topic => $topicProps) {
+                Topic::factory()
+                    ->withLocalization(Locale::Ru, $topicProps['localization'])
+                    ->create([
+                        'name' => $topic,
+                        'slug' => Str::slug($topic),
+                        'icon' => 'topic/' . $topicProps['icon'],
+                        'category_id' => $category,
+                    ]);
             }
         }
     }
@@ -34,50 +39,155 @@ class TopicsSeeder extends Seeder
     {
         return [
             'Development' => [
-                'Programming' => 'programming.png',
-                'Information security' => 'information-security.png',
-                'Machine learning' => 'machine-learning.png',
-                'Game development' => 'game-development.png',
-                'Algorithms' => 'algorithms.png',
+                'localization' => ['name' => 'Разработка'],
+                'topics' => [
+                    'Programming' => [
+                        'icon' => 'programming.png',
+                        'localization' => ['name' => 'Программирование'],
+                    ],
+                    'Information security' => [
+                        'icon' => 'information-security.png',
+                        'localization' => ['name' => 'Информационная безопасность'],
+                    ],
+                    'Machine learning' => [
+                        'icon' => 'machine-learning.png',
+                        'localization' => ['name' => 'Машинное обучение'],
+                    ],
+                    'Game development' => [
+                        'icon' => 'game-development.png',
+                        'localization' => ['name' => 'Разработка игр'],
+                    ],
+                    'Algorithms' => [
+                        'icon' => 'algorithms.png',
+                        'localization' => ['name' => 'Алгоритмы'],
+                    ],
+                ],
             ],
 
             'Admin' => [
-                'Network technologies' => 'network-technologies.png',
-                'Configuring Linux' => 'configuring-linux.png',
-                'DevOps' => 'devops.png',
-                'Server administration' => 'server-administration.png',
-                'Database administration' => 'database-administration.png',
+                'localization' => ['name' => 'Администрирование'],
+                'topics' => [
+                    'Network technologies' => [
+                        'localization' => ['name' => 'Сетевые технологии'],
+                        'icon' => 'network-technologies.png',
+                    ],
+                    'Configuring Linux' => [
+                        'localization' => ['name' => 'Настройка Linux'],
+                        'icon' => 'configuring-linux.png',
+                    ],
+                    'DevOps' => [
+                        'localization' => ['name' => 'DevOps'],
+                        'icon' => 'devops.png',
+                    ],
+                    'Server administration' => [
+                        'localization' => ['name' => 'Серверное администрирование'],
+                        'icon' => 'server-administration.png',
+                    ],
+                    'Database administration' => [
+                        'localization' => ['name' => 'Администрирование баз данных'],
+                        'icon' => 'database-administration.png',
+                    ],
+                ],
             ],
 
             'Design' => [
-                'Graphic design' => 'graphic-design.png',
-                'Web design' => 'web-design.png',
-                'Game design' => 'game-design.png',
-                'Typography' => 'typography.png',
+                'localization' => ['name' => 'Дизайн'],
+                'topics' => [
+                    'Graphic design' => [
+                        'localization' => ['name' => 'Графический дизайн'],
+                        'icon' => 'graphic-design.png',
+                    ],
+                    'Web design' => [
+                        'localization' => ['name' => 'Веб-дизайн'],
+                        'icon' => 'web-design.png',
+                    ],
+                    'Game design' => [
+                        'localization' => ['name' => 'Дизайн игр'],
+                        'icon' => 'game-design.png',
+                    ],
+                    'Typography' => [
+                        'localization' => ['name' => 'Типографика'],
+                        'icon' => 'typography.png',
+                    ],
+                ],
             ],
 
             'Management' => [
-                'IT-companies' => 'it-companies.png',
-                'Project management' => 'project-management.png',
-                'Product management' => 'product-management.png',
-                'Business models' => 'business-models.png',
-                'Freelance' => 'freelance.png',
-                'Agile' => 'agile.png',
+                'localization' => ['name' => 'Менеджмент'],
+                'topics' => [
+                    'IT-companies' => [
+                        'localization' => ['name' => 'IT-компании'],
+                        'icon' => 'it-companies.png',
+                    ],
+                    'Project management' => [
+                        'localization' => ['name' => 'Управление проектами'],
+                        'icon' => 'project-management.png',
+                    ],
+                    'Product management' => [
+                        'localization' => ['name' => 'Управление продуктом'],
+                        'icon' => 'product-management.png',
+                    ],
+                    'Business models' => [
+                        'localization' => ['name' => 'Бизнес-модели'],
+                        'icon' => 'business-models.png',
+                    ],
+                    'Freelance' => [
+                        'localization' => ['name' => 'Фриланс'],
+                        'icon' => 'freelance.png',
+                    ],
+                    'Agile' => [
+                        'localization' => ['name' => 'Agile'],
+                        'icon' => 'agile.png',
+                    ],
+                ],
             ],
 
             'Marketing' => [
-                'Web analytics' => 'web-analytics.png',
-                'Content marketing' => 'content-analytics.png',
-                'Branding' => 'branding.png',
-                'IT systems monetization' => 'it-monetization.png',
+                'localization' => ['name' => 'Маркетинг'],
+                'topics' => [
+                    'Web analytics' => [
+                        'localization' => ['name' => 'Веб-аналитика'],
+                        'icon' => 'web-analytics.png',
+                    ],
+                    'Content marketing' => [
+                        'localization' => ['name' => 'Контент-маркетинг'],
+                        'icon' => 'content-analytics.png',
+                    ],
+                    'Branding' => [
+                        'localization' => ['name' => 'Брендинг'],
+                        'icon' => 'branding.png',
+                    ],
+                    'IT systems monetization' => [
+                        'localization' => ['name' => 'Монетизация IT-систем'],
+                        'icon' => 'it-monetization.png',
+                    ],
+                ],
             ],
 
             'PopSci' => [
-                'Old hardware' => 'old-hardware.png',
-                'History of IT' => 'it-history.png',
-                'Software' => 'software.png',
-                'CPU' => 'cpu.png',
-                'Games and game consoles' => 'games.png',
+                'localization' => ['name' => 'Научпоп'],
+                'topics' => [
+                    'Old hardware' => [
+                        'localization' => ['name' => 'Старое железо'],
+                        'icon' => 'old-hardware.png',
+                    ],
+                    'History of IT' => [
+                        'localization' => ['name' => 'История IT'],
+                        'icon' => 'it-history.png',
+                    ],
+                    'Software' => [
+                        'localization' => ['name' => 'Софт'],
+                        'icon' => 'software.png',
+                    ],
+                    'CPU' => [
+                        'localization' => ['name' => 'Процессоры'],
+                        'icon' => 'cpu.png',
+                    ],
+                    'Games and game consoles' => [
+                        'localization' => ['name' => 'Игры и игровые консоли'],
+                        'icon' => 'games.png',
+                    ],
+                ],
             ],
         ];
     }
