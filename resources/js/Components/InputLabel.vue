9206b294d21e2c0869491455732fdb488435f644
Evgeny Kuzmin <skchel174@gmail.com>
2024-01-31 22:00:54 +0500

    Added ButtonTextInput component to article settings

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -7,7 +7,7 @@ defineProps({
 </script>
 
 <template>
-  <label class="font-medium text-sm text-gray-700">
+  <label class="font-semibold text-sm text-gray-700">
     <span v-if="value">{{ value }}</span>
     <span v-else><slot/></span>
   </label>
