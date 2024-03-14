a7823056a80fb2a625dabf4900cde0174c3fc848
Evgeny Kuzmin <skchel174@gmail.com>
2024-03-14 14:21:33 +0500

    Added to create_users_table migration avatar column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('new_email')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->string('status', 30);
+            $table->string('avatar')->nullable();
             $table->string('gender', 30)->nullable();
             $table->date('birthday')->nullable();
-            $table->rememberToken();
             $table->string('verify_token', 100)->unique()->nullable();
             $table->timestamp('verify_token_created_at')->nullable();
             $table->timestamp('login_at')->useCurrent();
