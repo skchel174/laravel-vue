4700fb8f2c8cf6917ece38b71a8776badf6e0a1d
Evgeny Kuzmin <skchel174@gmail.com>
2024-06-14 19:08:25 +0500

    create user factory

---
+++ database/factories/User/UserFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories\User;
+
+use App\Models\User\Status;
+use App\Models\User\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends Factory<User>
+ */
+class UserFactory extends Factory
+{
+    const PASSWORD = 'password';
+
+    public function definition(): array
+    {
+        return [
+            'email' => fake()->unique()->safeEmail(),
+            'username' => fake()->unique()->word(),
+            'password' => Hash::make(static::PASSWORD),
+            'status' => Status::Active,
+        ];
+    }
+}
