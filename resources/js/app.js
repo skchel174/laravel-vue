4cdd242070ce7c54314de9c34f68a6c2ba0ab768
Evgeny Kuzmin <skchel174@gmail.com>
2024-06-14 15:02:04 +0500

    removed default project files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,32 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  progress: {
+    color: '#4B5563',
+  },
+
+  title(title) {
+    return `${title} - ${import.meta.env.VITE_APP_NAME}`;
+  },
+
+  resolve(name) {
+    return resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'));
+  },
+
+  setup({el, App, props, plugin}) {
+    const root = {
+      render: () => h(App, props)
+    };
+
+    return createApp(root)
+      .use(plugin)
+      .use(ZiggyVue)
+      .mount(el);
+  },
 });
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 3000,
-        open: false,
-    },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: false,
+  },
 
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
 });
