432e2a3fde5ff000e719bcdca7c821d38b0bd379
Evgeny Kuzmin <skchel174@gmail.com>
2024-06-19 19:54:42 +0500

    add localization button to auth layout

---
+++ resources/js/Components/VIcon.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <span class="material-icons select-none">
+    {{ name }}
+  </span>
+</template>
