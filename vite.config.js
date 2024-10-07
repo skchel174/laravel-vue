537b0fa1160b2e596a17b6e06772aea865c0c7eb
Evgeny Kuzmin <skchel174@gmail.com>
2024-10-07 22:54:33 +0500

    Add environment variables to docker services

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,33 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite'
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+export default ({mode}) => {
+  const env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  };
+
+  return defineConfig({
     server: {
-        host: '0.0.0.0',
-        port: 3000,
-        open: false,
+      host: env.VITE_APP_HOST,
+      port: env.VITE_APP_PORT,
+      open: false,
     },
 
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-});
+      laravel({
+        input: 'resources/js/app.js',
+        refresh: true,
+      }),
+
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
+        },
+    }),],
+  });
+};
