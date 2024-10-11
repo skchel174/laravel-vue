c9e9d9addfed26356fd7d32fd32722561d492d7b
Evgeny Kuzmin <skchel174@gmail.com>
2024-10-11 21:14:40 +0500

    Removed breeze auth controllers and components

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,12 +2,6 @@
 import {Head, Link} from '@inertiajs/vue3';
 
 defineProps({
-  canLogin: {
-    type: Boolean,
-  },
-  canRegister: {
-    type: Boolean,
-  },
   laravelVersion: {
     type: String,
     required: true,
@@ -28,6 +22,7 @@ function handleImageError() {
 
 <template>
   <Head title="Welcome"/>
+
   <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
     <img
       id="background"
@@ -52,32 +47,6 @@ function handleImageError() {
               />
             </svg>
           </div>
-          <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-            <Link
-              v-if="$page.props.auth.user"
-              :href="route('dashboard')"
-              class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-            >
-              Dashboard
-            </Link>
-
-            <template v-else>
-              <Link
-                :href="route('login')"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-              >
-                Log in
-              </Link>
-
-              <Link
-                v-if="canRegister"
-                :href="route('register')"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-              >
-                Register
-              </Link>
-            </template>
-          </nav>
         </header>
 
         <main class="mt-6">
