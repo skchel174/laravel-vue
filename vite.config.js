90343972131b1aa0638d0be6a5914f855f29bf7c
Evgeny Kuzmin <skchel174@gmail.com>
2025-01-02 21:00:38 +0500

    Installed Inertia with vue and tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        hmr: {
+            host: 'localhost',
+            port: 3000,
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
