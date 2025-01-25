650a260ce4dc67fc7543d97d96dce468f1d7a198
Evgeny Kuzmin <skchel174@gmail.com>
2025-01-25 19:01:00 +0500

    Create users table migration

---
+++ database/migrations/2025_01_19_152824_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User\Gender;
+use App\Models\User\Status;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('username', 25)->unique();
+            $table->string('fullname', 60)->nullable();
+            $table->string('bio', 150)->nullable();
+            $table->string('location', 100)->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('gender', array_column(Gender::cases(), 'value'))->nullable();
+            $table->enum('status', array_column(Status::cases(), 'value'));
+            $table->string('avatar');
+            $table->rememberToken();
+            $table->string('verify_token', 100)->nullable();
+            $table->timestamp('verify_token_expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
