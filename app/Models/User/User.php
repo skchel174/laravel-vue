01e29712d80ec26aee48309d7ffa2517c7cd1b8a
Evgeny Kuzmin <skchel174@gmail.com>
2025-02-02 20:51:51 +0500

    Updated user model, created VerifyTokenCast and EmailCast

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -1,48 +1,69 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models\User;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\User\Casts\EmailCast;
+use App\Models\User\Casts\VerifyTokenCast;
+use Carbon\CarbonImmutable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableInterface;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+/**
+ * @property-read int $id
+ * @property string $email
+ * @property string $username
+ * @property string $password
+ * @property Status $status
+ * @property string $fullname
+ * @property string $bio
+ * @property string $location
+ * @property Gender $gender
+ * @property CarbonImmutable|null $birth_date
+ * @property string|null $verify_token
+ * @property CarbonImmutable $created_at
+ * @property CarbonImmutable $updated_at
+ */
+class User extends Model implements AuthenticatableInterface, AuthorizableInterface
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Authenticatable, Authorizable, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
         'email',
+        'username',
+        'fullname',
         'password',
+        'verify_token',
+        'status',
+        'bio',
+        'location',
+        'gender',
+        'birth_date',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
+        'verify_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'status' => Status::class,
+            'gender' => Gender::class,
+            'email' => EmailCast::class,
+            'verify_token' => VerifyTokenCast::class,
             'password' => 'hashed',
+            'birth_date' => 'immutable_datetime',
+            'created_at' => 'immutable_datetime',
+            'updated_at' => 'immutable_datetime',
         ];
     }
 }
