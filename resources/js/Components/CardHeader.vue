6740aa892f26d90530df3393bf606fdae2c93ee1
Evgeny Kuzmin <skchel174@gmail.com>
2025-04-13 20:39:55 +0500

    Create Card components

---
+++ resources/js/Components/CardHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="flex flex-col space-y-1 p-6 pb-0">
+    <slot />
+  </header>
+</template>
---
+++ resources/js/Components/CardTitle.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  component: {
+    type: String,
+    default: 'h3',
+    validator(value) {
+      return ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(value);
+    },
+  },
+});
+</script>
+
+<template>
+  <component :is="component" class="text-base leading-[1.5] font-semibold">
+    <slot />
+  </component>
+</template>
