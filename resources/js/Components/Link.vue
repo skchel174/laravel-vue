0aa4ad31bfcec33f7db57b7615f2af220240324d
Evgeny Kuzmin <skchel174@gmail.com>
2025-04-13 20:48:09 +0500

    Create Link component

---
+++ resources/js/Components/Link.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Link as InertiaLink } from '@inertiajs/vue3';
+</script>
+
+<template>
+  <InertiaLink
+    class="hover:text-gray-950 hover:underline underline-offset-4"
+  >
+    <slot />
+  </InertiaLink>
+</template>
